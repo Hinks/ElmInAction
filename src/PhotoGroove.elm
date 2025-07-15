57f5d4257ae306058a6b7005d3a69cff534102ff
Hinks <henrik.nr10@gmail.com>
2025-07-15 14:40:45 +0200

    Restructure code

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -31,132 +31,14 @@ import Json.Encode as Encode
 import Random
 
 
-urlPrefix : String
-urlPrefix =
-    "http://elm-in-action.com/"
+
+-- MODEL
 
 
 type ThumbnailSize
     = Small
     | Medium
     | Large
-
-
-type Msg
-    = ClickedPhoto String
-    | ClickedSize ThumbnailSize
-    | ClickedSurpriseMe
-    | GotRandomPhoto Photo
-    | GotPhotos (Result Http.Error (List Photo))
-    | SlidHue Int
-    | SlidRipple Int
-    | SlidNoise Int
-    | GotActivity String
-
-
-view : Model -> Html Msg
-view model =
-    div [ class "content" ] <|
-        case model.status of
-            Loaded photos selectedUrl ->
-                viewLoaded photos selectedUrl model.chosenSize model
-
-            Loading ->
-                []
-
-            Errored errorMessage ->
-                [ text ("Error: " ++ errorMessage) ]
-
-
-viewFilter : (Int -> Msg) -> String -> Int -> Html Msg
-viewFilter toMsg name magnitude =
-    div [ class "filter-slider" ]
-        [ label [] [ text name ]
-        , rangeSlider
-            [ Attr.max "11"
-            , Attr.property "val" (Encode.int magnitude)
-            , onSlide toMsg
-            ]
-            []
-        , label [] [ text (String.fromInt magnitude) ]
-        ]
-
-
-viewLoaded : List Photo -> String -> ThumbnailSize -> Model -> List (Html Msg)
-viewLoaded photos selectedUrl chosenSize model =
-    [ h1 [] [ text "Photo Groove" ]
-    , button
-        [ onClick ClickedSurpriseMe ]
-        [ text "Surprise Me!" ]
-    , div [ class "activity" ] [ text model.activity ]
-    , div [ class "filters" ]
-        [ viewFilter SlidHue "Hue" model.hue
-        , viewFilter SlidRipple "Ripple" model.ripple
-        , viewFilter SlidNoise "Noise" model.noise
-        ]
-    , h3 [] [ text "Thumbnail Size:" ]
-    , div [ id "choose-size" ]
-        (List.map viewSizeChooser [ Small, Medium, Large ])
-    , div [ id "thumbnails", class (sizeToString chosenSize) ]
-        (List.map (viewThumbnail selectedUrl) photos)
-    , canvas [ id "main-canvas", class "large" ] []
-    ]
-
-
-viewThumbnail : String -> Photo -> Html Msg
-viewThumbnail selectedUrl thumb =
-    img
-        [ src (urlPrefix ++ thumb.url)
-        , title (thumb.title ++ "[" ++ String.fromInt thumb.size ++ " kb]")
-        , classList [ ( "selected", selectedUrl == thumb.url ) ]
-        , onClick (ClickedPhoto thumb.url)
-        ]
-        []
-
-
-viewSizeChooser : ThumbnailSize -> Html Msg
-viewSizeChooser size =
-    label []
-        [ input
-            [ type_ "radio"
-            , name "size"
-            , onClick (ClickedSize size)
-            ]
-            []
-        , text (sizeToString size)
-        ]
-
-
-sizeToString : ThumbnailSize -> String
-sizeToString size =
-    case size of
-        Small ->
-            "small"
-
-        Medium ->
-            "med"
-
-        Large ->
-            "large"
-
-
-photoDecoder : Decoder Photo
-photoDecoder =
-    succeed buildPhoto
-        |> required "url" string
-        |> required "size" int
-        |> optional "title" string "(untitled)"
-
-
-buildPhoto : String -> Int -> String -> Photo
-buildPhoto url size title =
-    { url = url, size = size, title = title }
-
-
-port setFilters : FilterOptions -> Cmd msg
-
-
-port activityChanges : (String -> msg) -> Sub msg
 
 
 type alias FilterOptions =
@@ -188,6 +70,11 @@ type alias Model =
     }
 
 
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
 initialModel : Model
 initialModel =
     { status = Loading
@@ -205,6 +92,22 @@ initialCmd =
         { url = "http://elm-in-action.com/photos/list.json"
         , expect = Http.expectJson GotPhotos (list photoDecoder)
         }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+    | GotRandomPhoto Photo
+    | GotPhotos (Result Http.Error (List Photo))
+    | SlidHue Int
+    | SlidRipple Int
+    | SlidNoise Int
+    | GotActivity String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -304,14 +207,142 @@ selectUrl url status =
             status
 
 
-main : Program Value Model Msg
-main =
-    Browser.element
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "content" ] <|
+        case model.status of
+            Loaded photos selectedUrl ->
+                viewLoaded photos selectedUrl model.chosenSize model
+
+            Loading ->
+                []
+
+            Errored errorMessage ->
+                [ text ("Error: " ++ errorMessage) ]
+
+
+viewFilter : (Int -> Msg) -> String -> Int -> Html Msg
+viewFilter toMsg name magnitude =
+    div [ class "filter-slider" ]
+        [ label [] [ text name ]
+        , rangeSlider
+            [ Attr.max "11"
+            , Attr.property "val" (Encode.int magnitude)
+            , onSlide toMsg
+            ]
+            []
+        , label [] [ text (String.fromInt magnitude) ]
+        ]
+
+
+viewLoaded : List Photo -> String -> ThumbnailSize -> Model -> List (Html Msg)
+viewLoaded photos selectedUrl chosenSize model =
+    [ h1 [] [ text "Photo Groove" ]
+    , button
+        [ onClick ClickedSurpriseMe ]
+        [ text "Surprise Me!" ]
+    , div [ class "activity" ] [ text model.activity ]
+    , div [ class "filters" ]
+        [ viewFilter SlidHue "Hue" model.hue
+        , viewFilter SlidRipple "Ripple" model.ripple
+        , viewFilter SlidNoise "Noise" model.noise
+        ]
+    , h3 [] [ text "Thumbnail Size:" ]
+    , div [ id "choose-size" ]
+        (List.map viewSizeChooser [ Small, Medium, Large ])
+    , div [ id "thumbnails", class (sizeToString chosenSize) ]
+        (List.map (viewThumbnail selectedUrl) photos)
+    , canvas [ id "main-canvas", class "large" ] []
+    ]
+
+
+viewThumbnail : String -> Photo -> Html Msg
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , title (thumb.title ++ "[" ++ String.fromInt thumb.size ++ " kb]")
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick (ClickedPhoto thumb.url)
+        ]
+        []
+
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input
+            [ type_ "radio"
+            , name "size"
+            , onClick (ClickedSize size)
+            ]
+            []
+        , text (sizeToString size)
+        ]
+
+
+rangeSlider : List (Attribute msg) -> List (Html msg) -> Html msg
+rangeSlider attributes children =
+    node "range-slider" attributes children
+
+
+onSlide : (Int -> msg) -> Attribute msg
+onSlide toMsg =
+    at [ "detail", "userSlidTo" ] int
+        |> Json.Decode.map toMsg
+        |> on "slide"
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+
+-- DECODER
+
+
+photoDecoder : Decoder Photo
+photoDecoder =
+    succeed buildPhoto
+        |> required "url" string
+        |> required "size" int
+        |> optional "title" string "(untitled)"
+
+
+buildPhoto : String -> Int -> String -> Photo
+buildPhoto url size title =
+    { url = url, size = size, title = title }
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    activityChanges GotActivity
+
+
+port setFilters : FilterOptions -> Cmd msg
+
+
+port activityChanges : (String -> msg) -> Sub msg
+
+
+
+-- MAIN
 
 
 init : Value -> ( Model, Cmd Msg )
@@ -328,18 +359,11 @@ init flags =
     ( { initialModel | activity = activity }, initialCmd )
 
 
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    activityChanges GotActivity
-
-
-rangeSlider : List (Attribute msg) -> List (Html msg) -> Html msg
-rangeSlider attributes children =
-    node "range-slider" attributes children
-
-
-onSlide : (Int -> msg) -> Attribute msg
-onSlide toMsg =
-    at [ "detail", "userSlidTo" ] int
-        |> Json.Decode.map toMsg
-        |> on "slide"
+main : Program Value Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
